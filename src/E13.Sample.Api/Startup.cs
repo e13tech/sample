21b73339ab6d8202d122927c15dbc1ab2bbba6fe
JJ Bussert <jj.bussert@gmail.com>
2021-02-24 08:50:05 -0600

    fixed a compile error from a previous presentation

--- src/E13.Sample.Api/Startup.cs
+++ src/E13.Sample.Api/Startup.cs
@@ -42,7 +42,7 @@ namespace E13.Sample.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.is())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
